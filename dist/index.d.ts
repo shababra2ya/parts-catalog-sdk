60bdcb509fc4c4f766f207097ede62f734df772f
MahmoodKhalil57 <mahmoodihabk@gmail.com>
2025-04-08 00:38:40 +0000

    delete dist

--- dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { Client as ClientType } from 'openapi-fetch';
-import type { paths } from './index.d';
-type Client = ClientType<paths>;
-declare const client: () => Promise<{
-    GET: Client["GET"];
-}>;
-export { client as partsCatalogClient };
